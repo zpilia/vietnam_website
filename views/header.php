80890d484af4d6dff2c6b96e4566fae9f146de38
zpilia <61697702+zpilia@users.noreply.github.com>
2023-06-17 22:03:34 +0200

    modif

--- views/header.php
+++ views/header.php
@@ -2,9 +2,9 @@
 
     <!-- titre -->
     <div class="row">
-        
-
-
+        <div class="col text-center align-self-center">
+            <h1 class="title-header mt-4 mb-4">Vietnam Website</h1>
+        </div>
     </div>
 
 
