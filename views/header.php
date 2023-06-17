cc9299ab0008c566430600f9ce3c9b507b4a19e9
zpilia <61697702+zpilia@users.noreply.github.com>
2023-06-17 22:22:15 +0200

    modif

--- views/header.php
+++ views/header.php
@@ -7,6 +7,43 @@
         </div>
     </div>
 
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- Container wrapper -->
+        <div class="container-fluid">
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left links -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="accueil.php">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="societe.php">Société</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="service.php">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="information.php">Information</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="galerie.php">Galerie</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="recrutement.php">Recrutement</a>
+                    </li>
+                </ul>
+                <!-- Left links -->
+
+            </div>
+            <!-- Collapsible wrapper -->
+        </div>
+        <!-- Container wrapper -->
+    </nav>
 
 
 
