305dd55b48337e83f367fca166650602ae87d374
zpilia <61697702+zpilia@users.noreply.github.com>
2023-06-18 16:32:43 +0200

    modif

--- views/header.php
+++ views/header.php
@@ -12,21 +12,60 @@
         <div class="container-fluid">
             <!-- Collapsible wrapper -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Links -->
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="accueil.php">Accueil</a>
-                </li>
-                <!-- Dropdown -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown Link</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Link 1</a>
-                        <a class="dropdown-item" href="#">Link 2</a>
-                        <a class="dropdown-item" href="#">Link 3</a>
-                    </div>
-                </li>
-            </ul>
-    </nav>
+                <!-- Links -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="accueil.php">Accueil</a>
+                    </li>
 
+                    <!-- Dropdown 1-->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Culture
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Coutume</a>
+                            <a class="dropdown-item" href="#">Religion</a>
+                    </li>
+                    <!-- End Dropdown -->
+
+                    <!-- Dropdown 2-->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Cuisine
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Spécialité(s) Vietnamienne(s)</a>
+                            <a class="dropdown-item" href="#">Recette(s)</a>
+                    </li>
+                    <!-- End Dropdown 2-->
+
+                    <!-- Dropdown 3-->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            A visiter
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Incontournable</a>
+                    </li>
+                    <!-- End Dropdown 3-->
+
+                    <!-- Dropdown 4-->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Histoire
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Famille royal</a>
+                            <a class="dropdown-item" href="#">Chornologie</a>
+                    </li>
+                    <!-- End Dropdown 4-->
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
