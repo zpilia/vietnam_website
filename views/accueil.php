4d7b2482a354df25d51fdf843b2c323cec11c0c4
zpilia <61697702+zpilia@users.noreply.github.com>
2023-06-18 22:20:59 +0200

    modif

--- views/accueil.php
+++ views/accueil.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../public/css/bootstrap.css">
         <link rel="stylesheet" href="../public/css/style.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
         <title>Accueil</title>
     </head>
 
--- views/coutume.php
+++ views/coutume.php
@@ -21,42 +21,12 @@
 
     <br>
 
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-        <div class="col">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img
-                                src="https://mdbcdn.b-cdn.net/wp-content/uploads/2020/06/vertical.webp"
-                                alt="Trendy Pants and Shoes"
-                                class="img-fluid rounded-start"
-                        />
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top" alt="Palm Springs Road"/>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                        This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                    </p>
-                </div>
-            </div>
-        </div>
-        
+    
+
+
+
+
+
     </body>
 
 
