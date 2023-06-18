5aa067d6cb07399e8289b39f2e45e275d4aadc9b
zpilia <61697702+zpilia@users.noreply.github.com>
2023-06-18 18:01:05 +0200

    modif

--- views/header.php
+++ views/header.php
@@ -13,7 +13,7 @@
             <!-- Collapsible wrapper -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Links -->
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav fw-bold me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link" href="accueil.php">Accueil</a>
                     </li>
