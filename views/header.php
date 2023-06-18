577b4b20dc0c8b4e7a6842d013877301f368dbc7
zpilia <61697702+zpilia@users.noreply.github.com>
2023-06-18 15:49:35 +0200

    modif

--- views/header.php
+++ views/header.php
@@ -7,101 +7,26 @@
         </div>
     </div>
 
-    <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light-white">
         <!-- Container wrapper -->
         <div class="container-fluid">
             <!-- Collapsible wrapper -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left links -->
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="accueil.php">Accueil</a>
-                    </li>
-                    <!-- Navbar dropdown 1-->
-                    <li class="nav-item dropdown">
-                        <a
-                                class="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdown"
-                                role="button"
-                                data-toggle="dropdown"
-                                aria-expanded="false"
-                        >
-                            Culture
-                        </a>
-                        <!-- Dropdown menu -->
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="coutume.php">Coutume</a></li>
-                            <li><a class="dropdown-item" href="religion.php">Religion</a></li>
-                        </ul>
-                    </li>
-                    <!-- end dropdown -->
-                    <!-- Navbar dropdown 2-->
-                    <li class="nav-item dropdown">
-                        <a
-                                class="nav-link dropdown-toggle"
-                                href="accueil.php"
-                                id="navbarDropdown"
-                                role="button"
-                                data-toggle="dropdown"
-                                aria-expanded="false"
-                        >
-                            Cuisine
-                        </a>
-                        <!-- Dropdown menu -->
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="specialite_viet.php">Spécialité(s) vietnamienne(s)</a></li>
-                            <li><a class="dropdown-item" href="recette.php">Recette(s)</a></li>
-                        </ul>
-                    </li>
-                    <!-- end dropdown -->
-                    <!-- Navbar dropdown 3-->
-                    <li class="nav-item dropdown">
-                        <a
-                                class="nav-link dropdown-toggle"
-                                href="accueil.php"
-                                id="navbarDropdown"
-                                role="button"
-                                data-toggle="dropdown"
-                                aria-expanded="false"
-                        >
-                            A visiter
-                        </a>
-                        <!-- Dropdown menu -->
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="incontournable.php">Incontournable</a></li>
-                        </ul>
-                    </li>
-                    <!-- end dropdown -->
-                    <!-- Navbar dropdown 4-->
-                    <li class="nav-item dropdown">
-                        <a
-                                class="nav-link dropdown-toggle"
-                                href="accueil.php"
-                                id="navbarDropdown"
-                                role="button"
-                                data-toggle="dropdown"
-                                aria-expanded="false"
-                        >
-                            Histoire
-                        </a>
-                        <!-- Dropdown menu -->
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="famille_royal.php">Famille royal</a></li>
-                            <li><a class="dropdown-item" href="chrono.php">Chrnologie</a></li>
-                        </ul>
-                    </li>
-                    <!-- end dropdown -->
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="contact.php">Contact</a>
-                    </li>
-                </ul>
-                <!-- Left links -->
-            </div>
-            <!-- Collapsible wrapper -->
-        </div>
-        <!-- Container wrapper -->
+            <!-- Links -->
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="accueil.php">Accueil</a>
+                </li>
+                <!-- Dropdown -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown Link</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                    </div>
+                </li>
+            </ul>
     </nav>
 
 </header>
