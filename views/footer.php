7f6088e7920fa2b02d4908070cab2c8cf04080f0
zpilia <61697702+zpilia@users.noreply.github.com>
2023-06-18 18:03:20 +0200

    modif

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
 <footer class="page-footer font-small mdb-color pt-4 fixed-bottom">
     <hr>
     <!-- Copyright -->
-    <div class="text-center p-3">
+    <div class="text-center p-3 fw-bold mb-2">
         © Copyright 2023 : Tous droits réservés
     </div>
     <!-- Copyright -->
