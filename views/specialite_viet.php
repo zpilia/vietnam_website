c924a670f4c369bab836c9933871872dfd6a2c71
Zoé Pilia <zpilia2@gmail.com>
2023-06-20 22:00:07 +0200

    Update specialite_viet.php

--- views/specialite_viet.php
+++ views/specialite_viet.php
@@ -25,10 +25,11 @@
     <!-- insertion entête -->
     <?php require 'header.php' ?>
 
-    <body 
+    <body
           style="background: linear-gradient(-45deg, #ffff00, #a7ddf1, #70db70);"
     >
 
+    <!-- Titre -->
     <div class="title"
          style="font-family: 'Georgia', 'Times New Roman', serif;
                 font-size: 40px;
@@ -39,37 +40,50 @@
 
     <br>
 
-    <!-- section ==> carousel avec chaque information + image  -->
+    <!-- section ==> carousel avec chaque plat + image  -->
     <section class="pt-5 pb-5">
         <div class="container">
+
+            <!-- bouton de contrôle -->
             <div class="row">
                 <div class="col"></div>
                 <div class="col-6 text-right">
-                    <a class="btn btn-primary mb-3 mr-1" href="#carouselExampleIndicators2" role="button" data-slide="prev">
+                    <a class="btn btn-primary mb-3 mr-1" href="#myCarousel" role="button" data-slide="prev">
                         <i class="fa fa-arrow-left"></i>
                     </a>
-                    <a class="btn btn-primary mb-3 " href="#carouselExampleIndicators2" role="button" data-slide="next">
+                    <a class="btn btn-primary mb-3 " href="#myCarousel" role="button" data-slide="next">
                         <i class="fa fa-arrow-right"></i>
                     </a>
                 </div>
-                <div class="col-12">
-                    <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
 
+                <!-- carousel -->
+                <div class="col-12">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner">
+
+                            <!-- 1er page de carousel -->
                             <div class="carousel-item active">
                                 <div class="row">
 
+                                    <!-- 1er post -->
                                     <div class="col-md-4 mb-3">
+                                        <!-- image -->
                                         <div class="card">
-                                            <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532781914607-2031eca2f00d?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=7c625ea379640da3ef2e24f20df7ce8d">
+                                            <img class="img-fluid" alt="pho" src="../public/img/pho_4.jpg">
+                                            <!-- texte -->
                                             <div class="card-body">
-                                                <h4 class="card-title">Special title treatment</h4>
-                                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-
+                                                <h4 class="card-title">Phở</h4>
+                                                <p class="card-text">
+                                                    Cette soupe à base de bouillon de viande et de nouilles de riz est aromatisée avec plusieurs épices et accompagnée de différents ingrédients.
+                                                    Tu peux avoir des feuilles de basilic thaï, du piment, du citron vert ou encore de la coriandre.
+                                                    Il se déguste de partout, du matin au soir. Tu en trouveras à tous les coins de rue !
+                                                </p>
                                             </div>
-
                                         </div>
                                     </div>
+                                    <!-- Fin 1er post -->
+
+                                    <!-- 2ème post -->
                                     <div class="col-md-4 mb-3">
                                         <div class="card">
                                             <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=42b2d9ae6feb9c4ff98b9133addfb698">
@@ -80,6 +94,9 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <!-- Fin 2ème post -->
+
+                                    <!-- 3ème post -->
                                     <div class="col-md-4 mb-3">
                                         <div class="card">
                                             <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a">
@@ -90,12 +107,15 @@
                                             </div>
                                         </div>
                                     </div>
-
+                                    <!-- Fin 3ème post -->
                                 </div>
                             </div>
+
+                            <!-- 2 page de carousel -->
                             <div class="carousel-item">
                                 <div class="row">
 
+                                    <!-- 1er post -->
                                     <div class="col-md-4 mb-3">
                                         <div class="card">
                                             <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532771098148-525cefe10c23?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3f317c1f7a16116dec454fbc267dd8e4">
@@ -104,46 +124,53 @@
                                                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
 
                                             </div>
-
                                         </div>
                                     </div>
+                                    <!-- Fin 1er post -->
+
+                                    <!-- 2ème post -->
                                     <div class="col-md-4 mb-3">
                                         <div class="card">
                                             <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532715088550-62f09305f765?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=ebadb044b374504ef8e81bdec4d0e840">
                                             <div class="card-body">
                                                 <h4 class="card-title">Special title treatment</h4>
                                                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-
                                             </div>
                                         </div>
                                     </div>
+                                    <!-- Fin 2ème post -->
+
+                                    <!-- 3ème post -->
                                     <div class="col-md-4 mb-3">
                                         <div class="card">
                                             <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1506197603052-3cc9c3a201bd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=0754ab085804ae8a3b562548e6b4aa2e">
                                             <div class="card-body">
                                                 <h4 class="card-title">Special title treatment</h4>
                                                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-
                                             </div>
                                         </div>
                                     </div>
-
+                                    <!-- Fin 2ème post -->
                                 </div>
                             </div>
+
+                            <!-- 3ème page de carousel -->
                             <div class="carousel-item">
                                 <div class="row">
 
+                                    <!-- 1er post -->
                                     <div class="col-md-4 mb-3">
                                         <div class="card">
                                             <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=ee8417f0ea2a50d53a12665820b54e23">
                                             <div class="card-body">
                                                 <h4 class="card-title">Special title treatment</h4>
                                                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-
                                             </div>
-
                                         </div>
                                     </div>
+                                    <!-- Fin 1er post -->
+
+                                    <!-- 2ème post -->
                                     <div class="col-md-4 mb-3">
                                         <div class="card">
                                             <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532777946373-b6783242f211?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=8ac55cf3a68785643998730839663129">
@@ -154,6 +181,9 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <!-- Fin 2ème post -->
+
+                                    <!-- 3ème post -->
                                     <div class="col-md-4 mb-3">
                                         <div class="card">
                                             <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532763303805-529d595877c5?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=5ee4fd5d19b40f93eadb21871757eda6">
@@ -163,6 +193,8 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <!-- Fin 1er post -->
+
                                 </div>
                             </div>
                         </div>
