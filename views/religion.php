f1949390930436b1ea89ac96b5abc212ac34ffb9
Zoé Pilia <zpilia2@gmail.com>
2023-06-21 18:34:01 +0200

    modif

--- views/religion.php
+++ views/religion.php
@@ -27,80 +27,162 @@
             background: linear-gradient(45deg, #ffff00, #a7ddf1, #70db70);"
     >
 
-    <!-- Titre -->
-    <div class="title"
-         style="font-family: 'Georgia', 'Times New Roman', serif;
-                    font-size: 40px;
-                    color: #e60000"
-    >
-        <h1 class="mt-5 fw-bold text-center">Les religions pratiquées</h1>
-    </div>
+        <!-- Titre -->
+        <div class="title"
+             style="font-family: 'Georgia', 'Times New Roman', serif;
+                        font-size: 40px;
+                        color: #e60000"
+        >
+            <h1 class="mt-5 fw-bold text-center">Les religions pratiquées</h1>
+        </div>
 
 
-    <!-- 1er container -->
-    <div class="container mt-5">
-        <!-- 1er post -->
-        <div class="row gx-5 wow fadeIn">
-            <!-- image -->
-            <div class="col-lg-6 my-auto">
-                <div class="view overlay my-auto">
-                    <img src="../public/img/culte_ancetre_3.jpg.png" class="img-fluid" alt="culte_ancetre">
-                    <div class="mask">
+        <!-- 1er container -->
+        <div class="container mt-5">
+            <!-- 1er post -->
+            <div class="row gx-5 wow fadeIn">
+                <!-- image -->
+                <div class="col-lg-6 my-auto">
+                    <div class="view overlay my-auto">
+                        <img src="../public/img/culte_ancetre_3.jpg.png" class="img-fluid" alt="culte_ancetre">
+                        <div class="mask">
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- information -->
-            <div class="col-lg-6 my-auto">
-                <h2 class="h2-responsive fw-bold text-center" style="font-family: 'Georgia', serif; color: #ff1a1a">
-                    Culte des ancêtres
-                </h2>
-                <hr>
-                <p style="font-family: 'Georgia', serif;
-                              text-align: justify;
-                              font-size: 100%;
-                              color: black;">
-                    Le culte des ancêtres est le trait le plus saillant de la vie spirituelle vietnamienne.
-                    Si un homme meurt sans descendance, si l’encens ne brûle plus sur l’autel, les âmes des disparus sont condamnées à une errance éternelle, faute d’être honorées aux dates anniversaires.
-                    <br><br>
-                    Dans chaque maison vietnamienne, l’autel des ancêtres occupe une place importante, il est le cœur du foyer.
-                    Lorsqu’un Vietnamien décède, les membres de sa famille se ceignent la tête d’un bandeau blanc, couleur du deuil dans ce pays.
-                </p>
+                <!-- information -->
+                <div class="col-lg-6 my-auto">
+                    <h2 class="h2-responsive fw-bold text-center" style="font-family: 'Georgia', serif; color: #ff1a1a">
+                        Culte des ancêtres
+                    </h2>
+                    <hr>
+                    <p style="font-family: 'Georgia', serif;
+                                  text-align: justify;
+                                  font-size: 100%;
+                                  color: black;">
+                        Le culte des ancêtres est le trait le plus saillant de la vie spirituelle vietnamienne.
+                        Si un homme meurt sans descendance, si l’encens ne brûle plus sur l’autel, les âmes des disparus sont condamnées à une errance éternelle, faute d’être honorées aux dates anniversaires.
+                        <br><br>
+                        Dans chaque maison vietnamienne, l’autel des ancêtres occupe une place importante, il est le cœur du foyer.
+                        Lorsqu’un Vietnamien décède, les membres de sa famille se ceignent la tête d’un bandeau blanc, couleur du deuil dans ce pays.
+                    </p>
+                </div>
             </div>
+            <!-- Fin 1er post-->
         </div>
-        <!-- Fin 1er post-->
-    </div>
-    <!-- Fin 1er container -->
+        <!-- Fin 1er container -->
 
-    <!-- container ligne séparation -->
-    <div class="container mt-4 mb-4">
-        <hr>
-    </div>
+        <!-- container ligne séparation -->
+        <div class="container mt-4 mb-4">
+            <hr>
+        </div>
 
-    <!-- 2ème container -->
+        <!-- 2ème container -->
+            <div class="container">
+                <!-- 2ème post -->
+                <div class="row gx-5 wow fadeIn">
+                    <!-- information -->
+                    <div class="col-lg-6 my-auto">
+                        <h2 class="h2-responsive fw-bold text-center" style="font-family: 'Georgia', serif; color: #ff1a1a">
+                            Bouddhisme
+                        </h2>
+                        <hr>
+                        <p style="font-family: 'Georgia', serif;
+                                  text-align: justify;
+                                  font-size: 100%;
+                                  color: black;">
+                            Le bouddhisme est largement défini non pas comme une religion mais plutôt comme une philosophie orientale, fondée en Inde par Siddhârta Gautama
+                            (vers 560-483 av. J.-C.), prince de la tribu des Sakya, connu sous le nom de Bouddha "l'Illuminé".
+                            <br><br>
+                            Le but de cette philosophie est de sortir du cycle des réincarnations et accéder au nirvana (affranchissement éternel de la succession d'épreuves qu'est la vie terrestre) ...
+                        </p>
+                    </div>
+                    <!-- image -->
+                    <div class="col-lg-5 my-auto">
+                        <div class="view overlay mx-5">
+                            <img src="../public/img/bouddhisme_2.jpg" class="img-fluid" alt="bouddhisme">
+                            <div class="mask">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Fin 2ème post-->
+            </div>
+        <!-- Fin 2ème container -->
+
+        <!-- container ligne séparation -->
         <div class="container">
-            <!-- 2ème post -->
+            <hr>
+        </div>
+
+        <!-- 3ème container -->
+        <div class="container">
+            <!-- 3ème post -->
+            <div class="row gx-5 wow fadeIn">
+                <!-- image -->
+                <div class="col-lg-6 my-auto">
+                    <div class="view overlay my-auto">
+                        <img src="../public/img/confucianisme.jpg" class="img-fluid my-5" alt="confucianisme">
+                        <div class="mask">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- information -->
+                <div class="col-lg-6 my-auto">
+                    <h2 class="h2-responsive fw-bold text-center" style="font-family: 'Georgia', serif; color: #ff1a1a">
+                        Confucianisme
+                    </h2>
+                    <hr>
+                    <p style="font-family: 'Georgia', serif;
+                                  text-align: justify;
+                                  font-size: 100%;
+                                  color: black;">
+                        Difficile de parler de religion transcendante avec le confucianisme qui ne se réfère à aucun dieu mais instaure une philosophie morale basée sur le respect de la
+                        hiérarchie familiale et sociale, la bonté naturelle de l’homme et sa capacité à se perfectionner.
+                        <br><br>
+                        Inventé par Confucius (551-479 av. J.-C.), le confucianisme ne se préoccupe pas des origines du monde, ni des fins dernières de l'homme, mais il édicte un code moral basé sur 5 vertus :
+                        l'humanisme, l'équité, l'urbanité, l'intelligence et l'honnêteté.
+                    </p>
+                </div>
+            </div>
+            <!-- Fin 1er post-->
+        </div>
+        <!-- Fin 3ème container -->
+
+        <!-- container ligne séparation -->
+        <div class="container">
+            <hr>
+        </div>
+
+        <!-- 4ème container -->
+        <div class="container mt-4">
+            <!-- 4ème post -->
             <div class="row gx-5 wow fadeIn">
                 <!-- information -->
                 <div class="col-lg-6 my-auto">
                     <h2 class="h2-responsive fw-bold text-center" style="font-family: 'Georgia', serif; color: #ff1a1a">
-                        Bouddhisme
+                        Taoïsme
                     </h2>
                     <hr>
                     <p style="font-family: 'Georgia', serif;
-                              text-align: justify;
-                              font-size: 100%;
-                              color: black;">
-                        Le bouddhisme est largement défini non pas comme une religion mais plutôt comme une philosophie orientale, fondée en Inde par Siddhârta Gautama
-                        (vers 560-483 av. J.-C.), prince de la tribu des Sakya, connu sous le nom de Bouddha "l'Illuminé".
+                                  text-align: justify;
+                                  font-size: 100%;
+                                  color: black;">
+                        Avant d'être une religion, le taoïsme est d'abord une très belle leçon de sagesse orientale élaborée par un Chinois nommé Lao-tseu.
                         <br><br>
-                        Le but de cette philosophie est de sortir du cycle des réincarnations et accéder au nirvana (affranchissement éternel de la succession d'épreuves qu'est la vie terrestre) ...
+                        Ainsi naquit le Tao Te King (Dao De Jing) ou Livre de la Voie et de la Vertu. Cet ouvrage de 5 000 caractères constitue l’un des plus petits livres de Sagesse qui soient
+                        par sa taille, mais l’un des plus grands par la qualité de ses pensées et réflexions ! C’est la bible du taoïsme.
+                        <br><br>
+                        Le monde est régi par 2 principes contradictoires complémentaires : le yin et le yang.
+                        Le yin représente le côté féminin, passif, réceptif, l’obscurité, et le mou. Le yang, lui, incarne au contraire le principe masculin, actif, créatif, la clarté et la dureté.
+                        L’harmonie du yin et du yang donne le Tao, qui est une mutation perpétuelle.
                     </p>
                 </div>
                 <!-- image -->
                 <div class="col-lg-5 my-auto">
-                    <div class="view overlay mx-5">
-                        <img src="../public/img/bouddhisme_2.jpg" class="img-fluid" alt="bouddhisme">
+                    <div class="view overlay my-auto">
+                        <img src="../public/img/taoïsme.jpg" class="img-fluid mx-5" alt="taoisme">
                         <div class="mask">
                         </div>
                     </div>
@@ -108,128 +190,46 @@
             </div>
             <!-- Fin 2ème post-->
         </div>
-    <!-- Fin 2ème container -->
+        <!-- Fin 4ème container -->
 
-    <!-- container ligne séparation -->
-    <div class="container">
-        <hr>
-    </div>
+        <!-- container ligne séparation -->
+        <div class="container">
+            <hr>
+        </div>
 
-    <!-- 3ème container -->
-    <div class="container">
-        <!-- 3ème post -->
-        <div class="row gx-5 wow fadeIn">
-            <!-- image -->
-            <div class="col-lg-6 my-auto">
-                <div class="view overlay my-auto">
-                    <img src="../public/img/confucianisme.jpg" class="img-fluid my-5" alt="confucianisme">
-                    <div class="mask">
+        <!-- 5ème container -->
+        <div class="container">
+            <!-- 3ème post -->
+            <div class="row gx-5 wow fadeIn">
+                <!-- image -->
+                <div class="col-lg-6 my-auto">
+                    <div class="view overlay my-auto">
+                        <img src="../public/img/christianisme_3.jpg" class="img-fluid my-5" alt="christianisme">
+                        <div class="mask">
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- information -->
-            <div class="col-lg-6 my-auto">
-                <h2 class="h2-responsive fw-bold text-center" style="font-family: 'Georgia', serif; color: #ff1a1a">
-                    Confucianisme
-                </h2>
-                <hr>
-                <p style="font-family: 'Georgia', serif;
-                              text-align: justify;
-                              font-size: 100%;
-                              color: black;">
-                    Difficile de parler de religion transcendante avec le confucianisme qui ne se réfère à aucun dieu mais instaure une philosophie morale basée sur le respect de la
-                    hiérarchie familiale et sociale, la bonté naturelle de l’homme et sa capacité à se perfectionner.
-                    <br><br>
-                    Inventé par Confucius (551-479 av. J.-C.), le confucianisme ne se préoccupe pas des origines du monde, ni des fins dernières de l'homme, mais il édicte un code moral basé sur 5 vertus :
-                    l'humanisme, l'équité, l'urbanité, l'intelligence et l'honnêteté.
-                </p>
-            </div>
-        </div>
-        <!-- Fin 1er post-->
-    </div>
-    <!-- Fin 3ème container -->
-
-    <!-- container ligne séparation -->
-    <div class="container">
-        <hr>
-    </div>
-
-    <!-- 4ème container -->
-    <div class="container mt-4">
-        <!-- 4ème post -->
-        <div class="row gx-5 wow fadeIn">
-            <!-- information -->
-            <div class="col-lg-6 my-auto">
-                <h2 class="h2-responsive fw-bold text-center" style="font-family: 'Georgia', serif; color: #ff1a1a">
-                    Taoïsme
-                </h2>
-                <hr>
-                <p style="font-family: 'Georgia', serif;
-                              text-align: justify;
-                              font-size: 100%;
-                              color: black;">
-                    Avant d'être une religion, le taoïsme est d'abord une très belle leçon de sagesse orientale élaborée par un Chinois nommé Lao-tseu.
-                    <br><br>
-                    Ainsi naquit le Tao Te King (Dao De Jing) ou Livre de la Voie et de la Vertu. Cet ouvrage de 5 000 caractères constitue l’un des plus petits livres de Sagesse qui soient
-                    par sa taille, mais l’un des plus grands par la qualité de ses pensées et réflexions ! C’est la bible du taoïsme.
-                    <br><br>
-                    Le monde est régi par 2 principes contradictoires complémentaires : le yin et le yang.
-                    Le yin représente le côté féminin, passif, réceptif, l’obscurité, et le mou. Le yang, lui, incarne au contraire le principe masculin, actif, créatif, la clarté et la dureté.
-                    L’harmonie du yin et du yang donne le Tao, qui est une mutation perpétuelle.
-                </p>
-            </div>
-            <!-- image -->
-            <div class="col-lg-5 my-auto">
-                <div class="view overlay my-auto">
-                    <img src="../public/img/taoïsme.jpg" class="img-fluid mx-5" alt="taoisme">
-                    <div class="mask">
-                    </div>
+                <!-- information -->
+                <div class="col-lg-6 my-auto">
+                    <h2 class="h2-responsive fw-bold text-center" style="font-family: 'Georgia', serif; color: #ff1a1a">
+                        Christianisme
+                    </h2>
+                    <hr>
+                    <p style="font-family: 'Georgia', serif;
+                                  text-align: justify;
+                                  font-size: 100%;
+                                  color: black;">
+                        Les catholiques forment la 2e communauté religieuse, le Vietnam étant le 2e pays catholique d'Asie (avec 7 à 8 % de la population) après les Philippines.
+                        <br><br>
+                        Depuis 1975, les catholiques vivent en liberté surveillée. Le culte est autorisé. Mais les messes et les ordinations sont soumises à l'autorisation du gouvernement.
+                        Depuis l’ouverture du pays et sa relative libéralisation politique, l’avenir semble moins sombre pour les catholiques vietnamiens.
+                    </p>
                 </div>
             </div>
+            <!-- Fin 1er post-->
         </div>
-        <!-- Fin 2ème post-->
-    </div>
-    <!-- Fin 4ème container -->
-
-    <!-- container ligne séparation -->
-    <div class="container">
-        <hr>
-    </div>
-
-    <!-- 5ème container -->
-    <div class="container">
-        <!-- 3ème post -->
-        <div class="row gx-5 wow fadeIn">
-            <!-- image -->
-            <div class="col-lg-6 my-auto">
-                <div class="view overlay my-auto">
-                    <img src="../public/img/christianisme_3.jpg" class="img-fluid my-5" alt="christianisme">
-                    <div class="mask">
-                    </div>
-                </div>
-            </div>
-
-            <!-- information -->
-            <div class="col-lg-6 my-auto">
-                <h2 class="h2-responsive fw-bold text-center" style="font-family: 'Georgia', serif; color: #ff1a1a">
-                    Christianisme
-                </h2>
-                <hr>
-                <p style="font-family: 'Georgia', serif;
-                              text-align: justify;
-                              font-size: 100%;
-                              color: black;">
-                    Les catholiques forment la 2e communauté religieuse, le Vietnam étant le 2e pays catholique d'Asie (avec 7 à 8 % de la population) après les Philippines.
-                    <br><br>
-                    Depuis 1975, les catholiques vivent en liberté surveillée. Le culte est autorisé. Mais les messes et les ordinations sont soumises à l'autorisation du gouvernement.
-                    Depuis l’ouverture du pays et sa relative libéralisation politique, l’avenir semble moins sombre pour les catholiques vietnamiens.
-                </p>
-            </div>
-        </div>
-        <!-- Fin 1er post-->
-    </div>
-    <!-- Fin 5ème container -->
+        <!-- Fin 5ème container -->
 
     </body>
 
