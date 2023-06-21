874ebd29b8a1e31b9f7050a1f6ae81daa9785879
Zoé Pilia <zpilia2@gmail.com>
2023-06-21 15:10:22 +0200

    Update specialite_viet.php

--- views/specialite_viet.php
+++ views/specialite_viet.php
@@ -247,7 +247,6 @@
     <?php require 'footer.php' ?>
 
     <!-- library JS-->
-    <script src="../public/js/main.js"></script>
     <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
     <!-- Boostrap 5-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
