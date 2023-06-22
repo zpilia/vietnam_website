46e8d644d12895c3e078715e282d6ad3b384f631
Zoé Pilia <zpilia2@gmail.com>
2023-06-22 15:34:10 +0200

    Update incontournable.php

--- views/incontournable.php
+++ views/incontournable.php
@@ -44,14 +44,15 @@
             <div class="card h-100">
                 <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers"/>
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                    <h5 class="card-title">Hanoi</h5>
                     <p class="card-text">
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
+                        La capitale vietnamienne est connue pour son ambiance si particulière, typiquement vietnamienne avec son agitation et son ébullition qui sur l’instant fascine le voyageur.
+                        Hanoi séduit par sa richesse culturelle dont le Temple de la Littérature, le lac Hoan Kiem, le Mausolée Ho Chi Minh ou son formidable musée d’Ethnographie ne sont que quelques-uns
+                        des sites culturels emblématiques de la ville.
+                        <br><br>
+                        On aime la frénésie de son vieux quartier dit des 36 corporations, ses petits marchés animés, son remarquable patrimoine architectural colonial et son incomparable cuisine encore
+                        meilleure lorsqu’elle se savoure à même le trottoir.
                     </p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
             </div>
         </div>
@@ -61,9 +62,6 @@
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
             </div>
         </div>
@@ -78,17 +76,46 @@
                         that equal height action.
                     </p>
                 </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
             </div>
         </div>
     </div>
 
-
-
-
-
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" class="card-img-top" alt="Los Angeles Skyscrapers"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top" alt="Palm Springs Road"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This card has even longer content than the first to show
+                        that equal height action.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 
     </body>
 
