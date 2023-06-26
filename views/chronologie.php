edee70c26a920d7f1f9b8fc92473feaf4219b0d2
Zoé Pilia <zpilia2@gmail.com>
2023-06-26 16:13:06 +0200

    Update chronologie.php

--- views/chronologie.php
+++ views/chronologie.php
@@ -64,7 +64,7 @@
     <?php require 'footer.php' ?>
 
     <!-- library JS-->
-    <script src="../public/js/main.js"></script>
+    <script type="text/javascript" src="../public/js/main.js"></script>
     <!-- Boostrap 5-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- Boostrap 4-->
