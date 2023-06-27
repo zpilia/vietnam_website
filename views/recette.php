8d2e612cf2a4ecdc260aee92c04848467a15d495
Zoé Pilia <zpilia2@gmail.com>
2023-06-27 21:11:03 +0200

    modif + ajout

--- views/recette.php
+++ views/recette.php
@@ -37,39 +37,18 @@
         <h1 class="mt-4 fw-bold text-center">Cuisinons ensemble !</h1>
     </div>
 
+
     <!-- 1er container -->
     <div class="container my-auto">
-        <br>
-        <div class="row justify-content-center my-auto">
-
-            <!-- image -->
-            <div class="col my-auto">
-                <img src="../public/img/bo_bun_4.webp" alt="bo_bun" class="img-fluid rounded mx-5" style="width:70%; height:auto">
-            </div>
-
-            <!-- texte -->
-            <div class="col my-auto">
-                <h4 class="mb-3" style="font-family: 'Georgia', serif;
+        <div class="title mt-5">
+            <h4 style="font-family: 'Georgia', serif;
                     font-weight: bold;
-                    text-align: left;
+                    text-align: center;
                     color: black"
-                >
-                    Bò bún (4 personnes)
-                </h4>
-                <p style="font-family: 'Georgia', serif;
-                          text-align: justify;
-                          font-size: 110%;
-                          color: black;"
-                >
-                    Le « Tet Nguyen Dan » (le Nouvel An Vietnamien) est célébré au même moment que le Nouvel An chinois, entre mi-janvier et mi-février, selon les années.
-                    Suivant les moyens de la famille, les festivités peuvent durer de 3 à 7 jours.
-                    <br><br>
-                    C'est l'occasion pour les vietnamiens de se retrouver autour de copieux repas et de mets festifs.
-                    De petites enveloppes rouges garnies de quelques billets sont également offertes aux plus jeunes et aux aînés, comme autant de promesses de la fortune à venir au cours de l'année.
-                </p>
-            </div>
+            >
+                Bò bún (4 personnes)
+            </h4>
         </div>
-    </div>
 
 
 
